a04676be4e87cbfffdb9869e70c84b49f45d0a2a
rweatherl-intera <125318842+rweatherl-intera@users.noreply.github.com>
2023-08-31 12:48:50 -0500

    mnw2 2014_2022 model_files added for processing_pumping_data_2023.py

---
+++ model_packages/hist_2014_2022/mnw2/clean_and_copy_file.sh
@@ -0,0 +1,7 @@
+rm *.ref *.bas *.lpf *.dis
+#ln -s ../../../model_files/flow_2014_2021/100hr3.nam .
+ln -s ../../../model_files/flow_2014_2022/*.ref .
+ln -s ../../../model_files/flow_2014_2022/100hr3.bas .
+ln -s ../../../model_files/flow_2014_2022/100hr3.dis .
+ln -s ../../../model_files/flow_2014_2022/100hr3.lpf .
+
