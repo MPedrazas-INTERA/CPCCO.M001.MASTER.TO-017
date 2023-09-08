30320af686a6ca6ffca38c056f1d44e837bca018
Michelle Pedrazas <81190203+MPedrazas-INTERA@users.noreply.github.com>
2023-09-08 17:36:33 -0500

    comparisonconc plots

--- scripts/py13_postprocess_concentrations_at_wells.py
+++ scripts/py13_postprocess_concentrations_at_wells.py
@@ -112,23 +112,83 @@ def plot_concentrations(df_conc, crvi, mode):
             plt.close()
     return None
 
+def plot_concentrations_ALL(myDict):
+    outputDir = os.path.join(cwd, 'output', 'concentration_plots', f"comparison_calib_2020_2023")
+    if not os.path.isdir(outputDir):
+        os.makedirs(outputDir)
+
+    wellDict = {'199-H3-25':"North_PT_SensorData", '199-H3-26':"North_PT_SensorData", '199-H3-27':"North_PT_SensorData", '199-H3-2A': "North_AWLN", '199-H4-12A': "North_Manual",
+       '199-H4-15A': "North_Manual", '199-H4-17':"North_PT_SensorData", '199-H4-18': "North_Manual", '199-H4-4':"North_PT_SensorData", '199-H4-5': "North_AWLN",
+       '199-H4-64': "North_Manual", '199-H4-65': "North_Manual", '199-H4-8': "North_AWLN", '199-H4-84': "North_AWLN", '199-H4-85': "North_Manual",
+       '199-H4-86':"North_PT_SensorData", '199-H4-88': "North_AWLN", '199-H4-89': "North_Manual"}
+
+    times['SPstart'] = pd.to_datetime(times['SPstart'])
+
+    for well in wellDict.keys():
+        print(well)
+        fig, ax = plt.subplots(figsize=(8, 5))
+        for sce in myDict.keys():
+            print(sce)
+            crvi = myDict[sce][0] #crvi observations
+            df_conc = myDict[sce][1] #simulated concs
+
+            ucn_toplot = df_conc[df_conc['SAMP_SITE_NAME'] == well]
+            data_toplot = crvi[crvi['NAME'] == well]
+            if sce == "calib_2014_2020":
+                ax.plot(pd.to_datetime(ucn_toplot["SAMP_DATE"]), ucn_toplot['WeightedConc'], label=f'Calibrated Model', color = "cornflowerblue")
+                ax.scatter(pd.to_datetime(data_toplot['SAMP_DATE_TIME']), data_toplot['STD_VALUE_RPTD'], zorder=10, label = f"Calib Obs", c = "grey", edgecolor="k", s=10)
+                ax.plot(pd.to_datetime(data_toplot['SAMP_DATE_TIME']), data_toplot['STD_VALUE_RPTD'], zorder=10, c = "grey", ls="--", alpha=0.7)
+            if sce == "calib_2014_2023":
+                ax.plot(pd.to_datetime(ucn_toplot["SAMP_DATE"]), ucn_toplot['WeightedConc'], label=f'Extended Model', color = "olive")
+                ax.scatter(pd.to_datetime(data_toplot['SAMP_DATE_TIME']), data_toplot['STD_VALUE_RPTD'], zorder=10, label = f"New Obs", c = "r", edgecolor="darkred", s=10)
+                ax.plot(pd.to_datetime(data_toplot['SAMP_DATE_TIME']), data_toplot['STD_VALUE_RPTD'], zorder=10, c = "r", ls="--", alpha=0.7)
+        ax.set_title(f'{wellDict[well]}: {well}')
+        ax.set_ylabel('Cr(VI) (ug/L)')
+        ax.minorticks_on()
+        ax.grid(which='major', linestyle='-',
+                linewidth='0.1', color='red')
+        ax.grid(which='minor', linestyle=':',
+                linewidth='0.1', color='black')
+        ax.legend()
+        plt.xticks(rotation=45)
+        # fig.tight_layout()
+        # ax.set_xlabel("Date")
+        ax.set_xlim(pd.to_datetime("2014-01-01"), pd.to_datetime("2023-07-31"))
+        plt.savefig(os.path.join(outputDir, f'{well}_MOD2OBS.png'))
+        plt.close()
+    return None
 
 if __name__ == "__main__":
 
     cwd = os.getcwd()
-    sce = 'calib_2014_2023'
+
     wells = pd.read_csv(os.path.join(cwd, 'input', 'monitoring_wells_coords_ij.csv'))
-    chemfile = os.path.join(os.path.dirname(cwd), 'data', 'hydrochemistry', 'H-North Rebound Study Sampling_DATA.xlsx')
-    chemdata, crvi_filt = read_chemdata(chemfile)
 
-    times = pd.read_csv(os.path.join(cwd, 'input', 'sp_2014_2023.csv'))
+    sces = ['calib_2014_2023', 'calib_2014_2020']
+    myDict = {}
+    for sce in sces:
+        if sce == 'calib_2014_2023':
+            times = pd.read_csv(os.path.join(cwd, 'input', 'sp_2014_2023.csv'))
+            chemfile = os.path.join(os.path.dirname(cwd), 'data', 'hydrochemistry', 'H-North Rebound Study Sampling_DATA.xlsx')
+            chemdata, crvi_filt = read_chemdata(chemfile)
+        if sce == "calib_2014_2020":
+            crvi_filt = pd.read_csv(os.path.join("output", "concentration_data", "Cr_obs_avg_bySPs.csv")) #chem data used to calibrate 2014-2020 model
+            crvi_filt.rename(columns={"SAMP_SITE_NAME":"NAME", "SAMP_DATE":"SAMP_DATE_TIME"}, inplace=True)
 
-    mode = "mod2obs" #"mod2obs"
-    if mode == "ucn":
-        ucnfile = os.path.join(os.path.dirname(cwd), 'mruns', f'{sce}', f'tran_{sce[-9:]}', 'MT3D001.UCN')
-        data, df_conc = process_ucn_file(ucnfile)
-        ntimes, nlay, nr, nc = data.shape
-    elif mode == "mod2obs":
-        df_conc = pd.read_csv(os.path.join(os.path.dirname(cwd), 'mruns', f'{sce}', f'tran_{sce[-9:]}', 'post_process', 'mod2obs_monitoring_wells', 'simulated_conc_mod2obs.csv'))
+        mode = "mod2obs" #"mod2obs"
+        if mode == "ucn":
+            ucnfile = os.path.join(os.path.dirname(cwd), 'mruns', f'{sce}', f'tran_{sce[-9:]}', 'MT3D001.UCN') #different UCN name for 2014_2020
+            data, df_conc = process_ucn_file(ucnfile)
+            ntimes, nlay, nr, nc = data.shape
+        elif mode == "mod2obs":
+            df_conc = pd.read_csv(os.path.join(os.path.dirname(cwd), 'mruns', f'{sce}', f'tran_{sce[-9:]}', 'post_process', 'mod2obs_monitoring_wells', 'simulated_conc_mod2obs.csv'))
+            # if sce == "calib_2014_2023":
+                # df_conc["WeightedConc"] = df_conc["WeightedConc"]/1000 ?
 
-    plot_concentrations(df_conc, crvi_filt, mode)
+        # plot_concentrations(df_conc, crvi_filt, mode)
+
+        if sce not in myDict.keys():
+            myDict[sce] = [crvi_filt, df_conc]
+        plot_concentrations_ALL(myDict) #only works for mode == "mod2obs" for now
+
+
