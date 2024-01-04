3554ba487c0a3caf5a84d3963613e826fe9d2b93
Robin Weatherl <RWeatherl@intera.com>
2024-01-04 14:42:18 -0600

    stomp model and postprocess files

---
+++ continuing_source_modeling/flow_2014_Oct2023/stomp/cr6/100-D-100_exv1/getconcs.sh
@@ -0,0 +1,11 @@
+## one line per plot file
+perl zplot2profile-100D.pl input plot.2103 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.2537 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.2914 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.3280 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.3657 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.4034 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.4777 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.5154 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.5751 1 1 cr6-concs-100D100exv1
+perl zplot2profile-100D.pl input plot.5846 1 1 cr6-concs-100D100exv1
---
+++ continuing_source_modeling/flow_2014_Oct2023/stomp/cr6/100-D-100_slope/cr6-concs-100D100slope-labels.gnu
@@ -0,0 +1,4 @@
+set label "\n rs" at 0,1.183750000E+02 left font "Arial,8" front
+set label "\n re" at 0,120.375 left font "Arial,8" front
+set label "\n re" at 0,1.201250000E+02 left font "Arial,8" front
+set label "\n hf" at 0,122.125 left font "Arial,8" front
